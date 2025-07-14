7a71bee459664f61b8b3fc3b93c9ecc72553c88d
DamianoCas <damianocastellano2004@gmail.com>
2025-07-14 11:35:59 +0200

    added delete events

--- routes/events.go
+++ routes/events.go
@@ -3,8 +3,10 @@ package routes
 import (
 	"net/http"
 	"strconv"
+
 	"example.com/investment-calulator/models"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/text/message"
 )
 
 func getEvents(context *gin.Context) {
@@ -83,4 +85,34 @@ func updateEvent(context *gin.Context) {
 		return
 	}
 	context.JSON(http.StatusOK, gin.H{"message": "Event updated successfully!"})
+}
+
+func deleteEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event."})
+		return
+	}
+
+	var deletedEvent models.Event
+	err = context.ShouldBindJSON(&deletedEvent)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse this request."})
+		return
+	}
+
+	err = event.Delete();
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not delete event."})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Event deleted Successfully!"})
 }
